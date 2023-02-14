0e3a0f1dd09b86a0b20d2cfcd557ebcaac53656e
MRiivik <mriivik@tktk.ee>
2023-02-14 18:12:21 +0200

    test_harjutus

---
+++ Test_Harjutus_2/Program.cs
@@ -0,0 +1,15 @@
+﻿string userPIN;
+Console.WriteLine("Enter your pin:");
+while (true)
+{
+    userPIN = Console.ReadLine();
+    if (userPIN == "1234")
+    {
+        Console.WriteLine("Welcome!");
+            break;
+    }
+    else
+    {
+        Console.WriteLine("Wrong PIN, try again.");
+    }
+}

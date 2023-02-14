35912387d7256473d8f68dcbed71206a620b4e0a
MRiivik <mriivik@tktk.ee>
2023-02-14 12:16:43 +0200

    add test_harjutus code

---
+++ Test_Harjutus/Program.cs
@@ -0,0 +1,18 @@
+﻿int num;
+// Kasutaja sisestab arvu
+Console.Write("Enter a number to check if it's even or odd:");
+
+num = Convert.ToInt32(Console.ReadLine());
+
+switch (num % 2)
+{
+    // If n%2 == 0
+    case 0:
+        Console.WriteLine(num + " is an even number");
+        break;
+
+    //Else if n%2 == 1
+    case 1:
+        Console.WriteLine(num + " is an odd number");
+        break;
+}
